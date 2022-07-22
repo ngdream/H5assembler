83107bb9c90005bc2096e0ecfa44aa4c47569a91
NGdream <nguewofoss@gmail.com>
2022-07-23 00:07:43 +0100

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,2 +1,7 @@
+
+print("made by elodream ")
 print("hello welcome on H5assembler");
+print("H5 assemble its make for help frontend developer to code without repeat the same !")
+
+
 Sys.cmd("pause");
